c1281e1a10bdfe98aa51df390547718126035f35
mytrang2002 <mytranglk521@gmail.com>
2023-02-22 09:20:02 +0700

    Dinh nghia cac route cho tai nguyen Contact

---
+++ app/controllers/contact.controller.js
@@ -0,0 +1,47 @@
+// exports.create = (req, res) => {
+//   res.send({ message: 'create handler' })
+// }
+
+exports.findAll = (req, res) => {
+  res.send({ message: ' findAll handler ' })
+}
+
+exports.findOne = (req, res) => {
+  res.send({ message: ' findOne handler ' })
+}
+
+exports.update = (req, res) => {
+  res.send({ message: ' update handler ' })
+}
+
+exports.delete = (req, res) => {
+  res.send({ message: ' delete handler ' })
+}
+
+exports.deleteAll = (req, res) => {
+  res.send({ message: ' deleteAll handler ' })
+}
+
+exports.findAllFavorite = (req, res) => {
+  res.send({ message: ' findAllFavorite handler ' })
+}
+
+const ContactService = require('../services/contact.service')
+const MongoDB = require('../utils/mongodb.util')
+const ApiError = require('../api-error')
+
+exports.create = async (req, res, next) => {
+  if (!req.body?.name) {
+    return next(new ApiError(400, 'Name can not be empty'))
+  }
+
+  try {
+    const contactService = new ContactService(MongoDB.client)
+    const document = await contactService.create(req.body)
+    return res.send(document)
+  } catch (error) {
+    return next(
+      new ApiError(500, 'An error occurred while creating the contact')
+    )
+  }
+}
